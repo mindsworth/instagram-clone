206fdd53a7912e485c76005537db153e416354fa
Chigoziem Nwaiwu <princegoziem@gmail.com>
2021-02-09 15:53:25 +0100

    Keep calm and commit

--- components/DarkModeSwitch.tsx
+++ components/DarkModeSwitch.tsx
@@ -1,16 +1,18 @@
-import { MoonIcon, SunIcon } from "@chakra-ui/icons";
-import { IconButton, useColorMode } from "@chakra-ui/react";
+import React from 'react';
+
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { IconButton, useColorMode } from '@chakra-ui/react';
 
 const DarkModeSwitch = () => {
-    const {colorMode, toggleColorMode} = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
 
-    return (
-<IconButton
-aria-label="Toggle Dark Switch"
-icon={colorMode === "dark" ? <SunIcon/> : <MoonIcon/>}
-onClick={toggleColorMode}
-/>
-    );
-}
+  return (
+    <IconButton
+      aria-label="Toggle Dark Switch"
+      icon={colorMode === 'dark' ? <SunIcon /> : <MoonIcon />}
+      onClick={toggleColorMode}
+    />
+  );
+};
 
 export default DarkModeSwitch;
