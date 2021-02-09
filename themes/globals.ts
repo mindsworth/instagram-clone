2494a72c6ad572e0110ac8b42434a6dfe3ee6bf7
Chigoziem Nwaiwu <princegoziem@gmail.com>
2021-02-09 15:08:59 +0100

    fix eslint and prettier

--- themes/globals.ts
+++ themes/globals.ts
@@ -1,17 +1,17 @@
-import { extendTheme } from "@chakra-ui/react"
-import { mode } from "@chakra-ui/theme-tools"
+import { extendTheme } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
 
 const globals = extendTheme({
-    styles: {
-      global: (props) => ({
-        body: {
-          fontFamily: "body",
-          color: mode("gray.800", "whiteAlpha.900")(props),
-          bg: mode("white", "gray.800")(props),
-          lineHeight: "base",
-        },
-      }),
-    },
-  })
+  styles: {
+    global: (props) => ({
+      body: {
+        fontFamily: 'body',
+        color: mode('gray.800', 'whiteAlpha.900')(props),
+        bg: mode('white', 'gray.800')(props),
+        lineHeight: 'base',
+      },
+    }),
+  },
+});
 
-  export default globals
+export default globals;
--- themes/index.ts
+++ themes/index.ts
@@ -1,8 +1,7 @@
 // 1. import `extendTheme` function
-import { extendTheme } from "@chakra-ui/react";
-import globals from "./globals";
-import {colors} from "./foundations";
-
+import { extendTheme } from '@chakra-ui/react';
+import globals from './globals';
+import { colors } from './foundations';
 
 // 3. extend the theme
 const theme = extendTheme({ colors, globals });
